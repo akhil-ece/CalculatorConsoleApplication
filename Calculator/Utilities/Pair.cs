fb45120a04cc4a521c4ac690389bf1ea167f4f04
helperstop <akhilnani14@gmail.com>
2022-09-03 13:23:37 +0530

    Core Logic Implemented

---
+++ Calculator/Utilities/Pair.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Utilities
+{
+    public class Pair
+    {
+        public Stack<int> stP;
+        public char sign;
+        public Pair(Stack<int> stP, char sign)
+        {
+            this.stP = stP;
+            this.sign = sign;
+        }   
+    }
+}
