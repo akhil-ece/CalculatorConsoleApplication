e852a35837d33a33c20085711c0a4e579b6a6345
Akhil <akhilnani14@gmail.com>
2022-09-03 17:38:34 +0530

    Added More tests

--- CalculatorUnitTests/CalculatorUnitTest.cs
+++ CalculatorUnitTests/CalculatorUnitTest.cs
@@ -11,13 +11,76 @@ namespace CalculatorUnitTests
         CalculatorLogics calculator = new CalculatorLogics();
         PreProcessingLogics preProcessingLogics = new PreProcessingLogics();
         [TestMethod]
-        public void SimpleInput()
+        public void PlusTest()
         {
             string sampleInputString = "4 plus 5";
             sampleInputString = preProcessingLogics.ReplaceOperatorNames(sampleInputString);
             double expected = 9;
             double actual = calculator.Calculate(sampleInputString);
-            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+            Assert.AreEqual(expected, actual, 0.001, "Incorrect Result");
+        }
+        [TestMethod]
+        public void MinusTest()
+        {
+            string sampleInputString = "4 minus 5";
+            sampleInputString = preProcessingLogics.ReplaceOperatorNames(sampleInputString);
+            double expected = -1;
+            double actual = calculator.Calculate(sampleInputString);
+            Assert.AreEqual(expected, actual, 0.001, "Incorrect Result");
+            sampleInputString = "4 - 5";
+            actual = calculator.Calculate(sampleInputString);
+            Assert.AreEqual(expected, actual, 0.001, "Incorrect Result");
+        }
+        [TestMethod]
+        public void DivideTest()
+        {
+            string sampleInputString = "4 divide 5";
+            sampleInputString = preProcessingLogics.ReplaceOperatorNames(sampleInputString);
+            double expected = 0.8;
+            double actual = calculator.Calculate(sampleInputString);
+            Assert.AreEqual(expected, actual, 0.001, "Incorrect Result");
+            sampleInputString = "4 / 5";
+            actual = calculator.Calculate(sampleInputString);
+            Assert.AreEqual(expected, actual, 0.001, "Incorrect Result");
+        }
+        [TestMethod]
+        public void IntoTest()
+        {
+            string sampleInputString = "4 into 5";
+            sampleInputString = preProcessingLogics.ReplaceOperatorNames(sampleInputString);
+            double expected = 20;
+            double actual = calculator.Calculate(sampleInputString);
+            Assert.AreEqual(expected, actual, 0.001, "Incorrect Result");
+            sampleInputString = "4 * 5";
+            actual = calculator.Calculate(sampleInputString);
+            Assert.AreEqual(expected, actual, 0.001, "Incorrect Result");
+        }
+        [TestMethod]
+        public void NegativeResult()
+        {
+            string sampleInputString = "4 plus 5 minus 45";
+            sampleInputString = preProcessingLogics.ReplaceOperatorNames(sampleInputString);
+            double expected = -36;
+            double actual = calculator.Calculate(sampleInputString);
+            Assert.AreEqual(expected, actual, 0.001, "Incorrect Result");
+        }
+        [TestMethod]
+        public void DivideMixedTest()
+        {
+            string sampleInputString = "4 plus 5 minus 45 divide 5";
+            sampleInputString = preProcessingLogics.ReplaceOperatorNames(sampleInputString);
+            double expected = 0;
+            double actual = calculator.Calculate(sampleInputString);
+            Assert.AreEqual(expected, actual, 0.001, "Incorrect Result");
+        }
+        [TestMethod]
+        public void DividePrecisionTest()
+        {
+            string sampleInputString = "10/3";
+            sampleInputString = preProcessingLogics.ReplaceOperatorNames(sampleInputString);
+            double expected = 3.333;
+            double actual = calculator.Calculate(sampleInputString);
+            Assert.AreEqual(expected, actual, 0.001, "Incorrect Result");
         }
         [TestMethod]
         public void MixedOperatorTest()
@@ -25,8 +88,8 @@ namespace CalculatorUnitTests
             string sampleInputString = "4 plus (5 minus 5) + 5 * 45 + 4 (minus 2)";
             sampleInputString = preProcessingLogics.ReplaceOperatorNames(sampleInputString);
             double expected = 231;
-            double actual = calculator.Calculate(sampleInputString);    
-            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+            double actual = calculator.Calculate(sampleInputString);
+            Assert.AreEqual(expected, actual, 0.001, "Incorrect Result");
         }
     }
 }
